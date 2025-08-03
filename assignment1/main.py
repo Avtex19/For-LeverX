c53388a8e50bc5456d58ab7b016590c1651bc98f
Avtex19 <atukaberadze19@gmail.com>
2025-08-03 13:29:24 +0200

    feat: implement basic semantic version parsing with regex

---
+++ assignment1/main.py
@@ -0,0 +1,40 @@
+import re
+from functools import total_ordering
+
+
+@total_ordering
+class Version:
+    _semver_regex = re.compile(
+        r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)"
+        r"(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?"
+        r"(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?$"
+    )
+
+    def __init__(self, version: str):
+        self.original = version
+        match = self._semver_regex.fullmatch(version)
+        if match:
+            self.major = int(match.group(1))
+            self.minor = int(match.group(2))
+            self.patch = int(match.group(3))
+
+
+
+def main():
+    to_test = [
+        ("1.0.0", "2.0.0"),
+        ("1.0.0", "1.42.0"),
+        ("1.2.0", "1.2.42"),
+        ("1.1.0-alpha", "1.2.0-alpha.1"),
+        ("1.0.1b", "1.0.10-alpha.beta"),
+        ("1.0.0-rc.1", "1.0.0"),
+    ]
+
+    for left, right in to_test:
+        assert Version(left) < Version(right), "le failed"
+        assert Version(right) > Version(left), "ge failed"
+        assert Version(right) != Version(left), "neq failed"
+
+
+if __name__ == "__main__":
+    main()
